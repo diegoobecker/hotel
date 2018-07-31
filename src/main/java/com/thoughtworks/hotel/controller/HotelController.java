caf5eb7c747fc9b3dddb25dc82bcb36de8832e8c
diegobeckertw <dbecker@thoughtworks.com>
2018-07-31 10:45:29 -0300

    Get hotel by id

--- src/main/java/com/thoughtworks/hotel/controller/HotelController.java
+++ src/main/java/com/thoughtworks/hotel/controller/HotelController.java
@@ -1,10 +1,12 @@
 package com.thoughtworks.hotel.controller;
 
+import com.thoughtworks.hotel.model.Hotel;
 import com.thoughtworks.hotel.repository.HotelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,11 @@ public class HotelController {
     @GetMapping
     public ResponseEntity<?> listAll() {
         return new ResponseEntity<>(hotelRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getHotelById(@PathVariable("id") Long id) {
+        Hotel hotel = hotelRepository.findById(id).get();
+        return new ResponseEntity<>(hotel, HttpStatus.OK);
     }
 }
